c6bc3645a825da4013b1461a8dfe23b6dbd828fc
Seshi Chemudugunta <seshi@Seshis-MacBook-Pro.local>
2015-06-23 23:48:11 +0900

    added johnny-five

--- main.js
+++ main.js
@@ -3,10 +3,8 @@ var app = express();
 var server = require('http').createServer(app);
 var path = require('path');
 
-//var GPIO = require('onoff').Gpio,
-//  green = new GPIO(14, 'out');
-
-var gpio = require('pi-gpio');
+var five = require('johnny-five');
+var board = new five.Board();
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
@@ -24,22 +22,17 @@ var server = app.listen(3000, function () {
 
 });
 
+board.on("ready", function() {
+  var led = new five.Led(14);
+  led.blink(500);
+});
+
 var io = require('socket.io')(server);
 io.on('connection', function(socket) { 
   console.log('user connected');
   
   socket.on('toggle', function() {
     console.log('toggle called');
-    
-//    var state = green.readSync();
-//    green.writeSync(Number(!state));
-    gpio.open(14, 'output', function (params) {
-      gpio.read(14, function(err, value){
-        console.log('pin value:' + value);
-        gpio.write(14, 1);
-      });
-    });
-    
     
   });
 });
