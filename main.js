aab9d6c8ca7babca23cb615a7c9b2f8270809a0f
Seshi Chemudugunta <seshi@Seshis-MacBook-Pro.local>
2015-06-25 08:04:04 +0900

    Fixed tab spacing in main.js

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-var express = require('express'); var app = express(); var server = 
-require('http').createServer(app); var path = require('path');
+var express = require('express'); 
+var app = express(); var server = 
+require('http').createServer(app); 
+var path = require('path');
 
 var raspi = require('raspi-io');
 var five = require('johnny-five');
-var board = new five.Board({
-  io: new raspi()
+
+var board = new five.board({
+	io: new raspi()
 });
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.use('/node_modules', express.static('node_modules'));
@@ -16,32 +19,30 @@ app.use('/app', express.static('app'));
 
 var server = app.listen(3000, function () {
 
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+	var host = server.address().address;
+	var port = server.address().port;
+	
+	console.log('Nodebot - Test Harness listening at http://%s:%s', host, port);
 });
 
 var led = undefined;
 var ledState = false;
 board.on("ready", function() {
-  led = new five.Led('P1-8');
-  led.off();
-//  led.blink(500);
+	led = new five.Led('P1-8');
+	led.off();
 });
 
 var io = require('socket.io')(server);
 io.on('connection', function(socket) { 
-  console.log('user connected');
+	console.log('user connected');
   
-  socket.on('toggle', function() {
-    console.log('toggle called');
-    
-    if (led !== undefined) {
-      console.log('ledState: ' + ledState);
-      ledState === true ? led.off() : led.on();
-      ledState = !ledState;      
-    }
-  });
+	socket.on('toggle', function() {
+		console.log('toggle called');
+		
+		if (led !== undefined) {
+			console.log('ledState: ' + ledState);
+			ledState === true ? led.off() : led.on();
+			ledState = !ledState;      
+		}
+	});
 });
